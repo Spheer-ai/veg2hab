a8d0832966ed72e14ae687674daddef7509ec36a
JRdeLange <jordy.de.lange>
2024-01-17 08:57:07 +0100

    cleaned up in preparation of PR

--- veg2hab/definitietabel.py
+++ veg2hab/definitietabel.py
@@ -44,99 +44,6 @@ class DefinitieTabel:
         df = pd.read_excel(path)
         return cls(df)
 
-    # NOTE: Ik heb wel veel static methods zo, kan dat netter?
-    @staticmethod
-    def opschonen_definitietabel(path_in: Path, path_out: Path):
-        """
-        Ontvangt een was-wordt lijst en output een opgeschoonde was-wordt lijst
-        """
-        # assert path in is an xlsx file
-        assert path_in.suffix == ".xls", "Input file is not an xls file"
-        # assert path out is an xlsx file
-        assert path_out.suffix == ".xlsx", "Output file is not an xlsx file"
-
-        dt = pd.read_excel(
-            path_in,
-            usecols=[
-                "Code habitat (sub)type",
-                "Goed / Matig",
-                "Code vegetatietype",
-                "beperkende criteria",
-                "alleen in mozaïek",
-            ],
-        )
-        # Hernoemen kolommen
-        dt = dt.rename(
-            columns={
-                "Code habitat (sub)type": "HABCODE",
-                "Goed / Matig": "Kwaliteit",
-                "Code vegetatietype": "VvN",
-                "beperkende criteria": "mits",
-                "alleen in mozaïek": "mozaiek",
-            }
-        )
-
-        # Verwijderen rijen met missende data
-        dt = dt.dropna(how="any")
-
-        # Verplaatsen SBB naar eigen kolom
-        SBB_mask = dt["VvN"].str.contains("SBB")
-        dt.loc[SBB_mask, "SBB"] = dt.loc[SBB_mask, "VvN"]
-        dt.loc[SBB_mask, "VvN"] = pd.NA
-
-        dt["SBB"] = DefinitieTabel.opschonen_SBB(dt["SBB"])
-        dt["VvN"] = DefinitieTabel.opschonen_VvN(dt["VvN"])
-
-        # Checken
-        assert SBB.validate_pandas_series(
-            dt["SBB"], print_invalid=True
-        ), "Niet alle SBB codes zijn valid"
-        assert VvN.validate_pandas_series(
-            dt["VvN"], print_invalid=True
-        ), "Niet alle VvN codes zijn valid"
-
-        dt.to_excel(path_out, index=False)
-
-    @staticmethod
-    def opschonen_SBB(dt_SBB: pd.Series):
-        """ """
-        dt_SBB = dt_SBB.astype("string")
-
-        # Verwijderen prefix
-        dt_SBB = dt_SBB.str.replace("SBB-", "")
-        # Verwijderen xxx suffix
-        dt_SBB = dt_SBB.str.replace("-xxx [08-f]", "", regex=False)
-        # Maak lowercase
-        dt_SBB = dt_SBB.str.lower()
-        # Regex vervang 0[1-9] door [1-9]
-        dt_SBB = dt_SBB.str.replace(r"0([1-9])", r"\1", regex=True)
-        
-        return dt_SBB
-
-    def check_validity_SBB(self, print_invalid: bool = False):
-        """
-        Checkt of de SBB codes valide zijn.
-        """
-        dt_SBB = self.df["SBB"].astype("string")
-
-        return SBB.validate_pandas_series(dt_SBB, print_invalid=print_invalid)
-
-    @staticmethod
-    def opschonen_VvN(dt_VvN: pd.Series):
-        """
-        Zet VvN codes om naar ons format
-        """
-        dt_VvN = dt_VvN.astype("string")
-
-        # Maak lowercase
-        dt_VvN = dt_VvN.str.lower()
-        # Verwijderen '-'
-        dt_VvN = dt_VvN.str.replace("-", "")
-        # Converteren rompgemeenschappen en derivaaatgemeenschappen
-        dt_VvN = dt_VvN.str.replace(r"\[.*\]", "", regex=True)
-
-        return dt_VvN
-
     def check_validity_VvN(self, print_invalid: bool = False):
         """
         Checkt of de VvN valide zijn.
@@ -144,3 +51,95 @@ class DefinitieTabel:
         dt_VvN = self.df["VvN"].astype("string")
 
         return VvN.validate_pandas_series(dt_VvN, print_invalid=print_invalid)
+
+    def check_validity_SBB(self, print_invalid: bool = False):
+        """
+        Checkt of de SBB valide zijn.
+        """
+        dt_SBB = self.df["SBB"].astype("string")
+
+        return SBB.validate_pandas_series(dt_SBB, print_invalid=print_invalid)
+
+
+def opschonen_definitietabel(path_in: Path, path_out: Path):
+    """
+    Ontvangt een was-wordt lijst en output een opgeschoonde was-wordt lijst
+    """
+    # assert path in is an xlsx file
+    assert path_in.suffix == ".xls", "Input file is not an xls file"
+    # assert path out is an xlsx file
+    assert path_out.suffix == ".xlsx", "Output file is not an xlsx file"
+
+    dt = pd.read_excel(
+        path_in,
+        usecols=[
+            "Code habitat (sub)type",
+            "Goed / Matig",
+            "Code vegetatietype",
+            "beperkende criteria",
+            "alleen in mozaïek",
+        ],
+    )
+    # Hernoemen kolommen
+    dt = dt.rename(
+        columns={
+            "Code habitat (sub)type": "HABCODE",
+            "Goed / Matig": "Kwaliteit",
+            "Code vegetatietype": "VvN",
+            "beperkende criteria": "mits",
+            "alleen in mozaïek": "mozaiek",
+        }
+    )
+
+    # Verwijderen rijen met missende data in VvN
+    dt = dt.dropna(subset=["VvN"])
+
+    # Verplaatsen SBB naar eigen kolom
+    SBB_mask = dt["VvN"].str.contains("SBB")
+    dt.loc[SBB_mask, "SBB"] = dt.loc[SBB_mask, "VvN"]
+    dt.loc[SBB_mask, "VvN"] = pd.NA
+
+    dt["SBB"] = opschonen_SBB(dt["SBB"])
+    dt["VvN"] = opschonen_VvN(dt["VvN"])
+
+    # Checken
+    assert SBB.validate_pandas_series(
+        dt["SBB"], print_invalid=True
+    ), "Niet alle SBB codes zijn valid"
+    assert VvN.validate_pandas_series(
+        dt["VvN"], print_invalid=True
+    ), "Niet alle VvN codes zijn valid"
+
+    dt.to_excel(path_out, index=False)
+
+
+def opschonen_SBB(dt_SBB: pd.Series):
+    """ """
+    dt_SBB = dt_SBB.astype("string")
+
+    # Verwijderen prefix
+    dt_SBB = dt_SBB.str.replace("SBB-", "")
+    # Verwijderen xxx suffix
+    dt_SBB = dt_SBB.str.replace("-xxx [08-f]", "", regex=False)
+    # Maak lowercase
+    dt_SBB = dt_SBB.str.lower()
+    # Regex vervang 0[1-9] door [1-9]
+    dt_SBB = dt_SBB.str.replace(r"0([1-9])", r"\1", regex=True)
+
+    return dt_SBB
+
+
+def opschonen_VvN(dt_VvN: pd.Series):
+    """
+    Zet VvN codes om naar ons format
+    """
+    dt_VvN = dt_VvN.astype("string")
+
+    # Maak lowercase
+    dt_VvN = dt_VvN.str.lower()
+    # Verwijderen '-'
+    dt_VvN = dt_VvN.str.replace("-", "")
+    # Converteren rompgemeenschappen en derivaaatgemeenschappen
+    dt_VvN = dt_VvN.str.replace(r"\[.*\]", "", regex=True)
+
+    return dt_VvN
