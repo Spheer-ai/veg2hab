26112d1e0f98f1b2b68a8b87081d6d0cf5fe9c37
JRdeLange <jordy.de.lange>
2024-03-28 08:30:52 +0100

    maybeboolean logic works

--- veg2hab/criteria.py
+++ veg2hab/criteria.py
@@ -1,4 +1,5 @@
 import json
+from functools import reduce
 from typing import ClassVar, List, Optional
 
 import geopandas as gpd
@@ -82,7 +83,7 @@ class PlaceholderCriterium(BeperkendCriterium):
     _evaluation: Optional[MaybeBoolean] = PrivateAttr(default=None)
 
     def check(self, row: gpd.GeoSeries) -> None:
-        self._evaluation = MaybeBoolean.FALSE
+        self._evaluation = MaybeBoolean.CANNOT_BE_AUTOMATED
 
     def __str__(self):
         return "placeholder"
@@ -139,11 +140,8 @@ class OfCriteria(BeperkendCriterium):
 
     @property
     def evaluation(self) -> MaybeBoolean:
-        return (
-            MaybeBoolean.TRUE
-            if any(crit.evaluation == MaybeBoolean.TRUE for crit in self.sub_criteria)
-            else MaybeBoolean.FALSE
-        )
+        assert len(self.sub_criteria) > 0, "OrCriteria zonder subcriteria"
+        return reduce(lambda x, y: x | y, (crit.evaluation for crit in self.sub_criteria), MaybeBoolean.FALSE)
 
     def __str__(self):
         of_crits = " of ".join(str(crit) for crit in self.sub_criteria)
@@ -165,12 +163,9 @@ class EnCriteria(BeperkendCriterium):
 
     @property
     def evaluation(self) -> MaybeBoolean:
-        return (
-            MaybeBoolean.TRUE
-            if all(crit.evaluation == MaybeBoolean.TRUE for crit in self.sub_criteria)
-            else MaybeBoolean.FALSE
-        )
-
+        assert len(self.sub_criteria) > 0, "EnCriteria zonder subcriteria"
+        return reduce(lambda x, y: x & y, (crit.evaluation for crit in self.sub_criteria), MaybeBoolean.TRUE)
+        
     def __str__(self):
         en_crits = " en ".join(str(crit) for crit in self.sub_criteria)
         return f"({en_crits})"
