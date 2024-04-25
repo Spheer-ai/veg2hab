97c4007f2791ed160354e564b54c219bbbf02a11
Mark Boer <markboer@spheer.ai>
2024-04-25 18:47:19 +0200

    create a cli for veg2hab

--- veg2hab/__main__.py
+++ veg2hab/__main__.py
@@ -7,7 +7,7 @@ import click
 import veg2hab
 from veg2hab import constants
 from veg2hab.definitietabel import opschonen_definitietabel
-from veg2hab.io.cli import AccessDBInputs, CLIInterface, ShapefileInputs
+from veg2hab.io.cli import CLIAccessDBInputs, CLIInterface, CLIShapefileInputs
 from veg2hab.waswordtlijst import opschonen_waswordtlijst
 
 
@@ -26,78 +26,23 @@ def main(verbose: int):
 
 
 @main.command(
-    name=AccessDBInputs.label,
-    help=AccessDBInputs.description,
+    name=CLIAccessDBInputs.label,
+    help=CLIAccessDBInputs.get_argument_description(),
 )
-def digitale_standaard():
-    pass
+@CLIAccessDBInputs.click_decorator
+def digitale_standaard(**kwargs):
+    print(kwargs)
+    params = CLIAccessDBInputs(**kwargs)
 
 
 @main.command(
-    name=ShapefileInputs.label,
-    help=ShapefileInputs.description,
+    name=CLIShapefileInputs.label,
+    help=CLIShapefileInputs.get_argument_description(),
 )
-def vector_bestand():
-    """"""
-    pass
-
-
-@click.group()
-def veg2hab_internal():
-    """Used for checking versions and preparing the python release."""
-
-
-@veg2hab_internal.command()
-def check_versions():
-    """Check that the versions in poetry, toolbox.py and __version__ match"""
-    # get the version of the software from poetry
-    poetry_version = (
-        subprocess.check_output(["poetry", "version", "--short"])
-        .decode("utf-8")
-        .strip()
-    )
-
-    if poetry_version != veg2hab.__version__:
-        raise ValueError(
-            f"Versions do not match: {poetry_version} != {veg2hab.__version__}"
-        )
-
-    import runpy
-
-    top_level_namespace = runpy.run_path(constants.TOOLBOX_PYT_PATH)
-
-    if poetry_version not in top_level_namespace["SUPPORTED_VERSIONS"]:
-        raise ValueError(
-            f"Version {poetry_version} is not in {top_level_namespace['SUPPORTED_VERSIONS']}"
-        )
-
-
-@veg2hab_internal.command()
-def create_package_data():
-    path_in_wwl = (
-        Path(__file__).parent
-        / ".."
-        / "data"
-        / "5. Was-wordt-lijst-vegetatietypen-en-habitattypen-09-02-2021.xlsx"
-    )
-    path_out_wwl = Path(__file__).parent / "package_data" / "opgeschoonde_waswordt.xlsx"
-    opschonen_waswordtlijst(path_in_wwl, path_out_wwl)
-
-    path_in_dt = (
-        Path(__file__).parent
-        / ".."
-        / "data"
-        / "definitietabel habitattypen (versie 24 maart 2009)_0.xls"
-    )
-    path_in_mitsjson = Path(__file__).parent / ".." / "data" / "mitsjson.csv"
-    path_in_mozaiekjson = Path(__file__).parent / ".." / "data" / "mozaiekjson.csv"
-    path_out_dt = (
-        Path(__file__).parent / "package_data" / "opgeschoonde_definitietabel.xlsx"
-    )
-    opschonen_definitietabel(
-        path_in_dt, path_in_mitsjson, path_in_mozaiekjson, path_out_dt
-    )
+@CLIShapefileInputs.click_decorator
+def vector_bestand(**kwargs):
+    params = CLIShapefileInputs(**kwargs)
 
 
 if __name__ == "__main__":
-    check_versions()
+    main()
