b06cac4847d9605a61a3bb06d54ca36051dca899
JRdeLange <jordy.de.lange>
2024-05-08 16:52:56 +0200

    ran black, fixed tests

--- tests/test_minimum_oppervlak.py
+++ tests/test_minimum_oppervlak.py
@@ -1,3 +1,5 @@
+import json
+import os
 from collections import defaultdict
 
 import geopandas as gpd
@@ -5,57 +7,70 @@ import pytest
 
 from veg2hab.enums import KeuzeStatus, Kwaliteit
 from veg2hab.habitat import HabitatKeuze, apply_minimum_oppervlak
+from veg2hab.io.cli import CLIInterface
 from veg2hab.vegkartering import VegTypeInfo
 
 #
 # Deze test is voor minimum oppervlak **zonder** enige vorm van functionele samenhang
 #
-# De minimum oppervlakken zijn in MIN_AREA nog even hier gedefinieerd (met dezelfde waarden als in habitat.py),
+# De minimum oppervlakken zijn in min_area nog even hier gedefinieerd (met dezelfde waarden als in habitat.py),
 # maar moeten in de toekomst naar een mooie config file.
 #
-
-# TODO: dit naar config
-
-MIN_AREA = defaultdict(lambda: 100)
-for habtype in ["H6110", "H7220"]:
-    MIN_AREA[habtype] = 10
-for habtype in [
-    "H2180_A",
-    "H2180_B",
-    "H2180_C",
-    "H9110",
-    "H9120",
-    "H9160_A",
-    "H9160_B",
-    "H9190",
-    "H91D0",
-    "H91E0_A",
-    "H91E0_B",
-    "H91E0_C",
-    "H91F0",
-]:
-    MIN_AREA[habtype] = 1000
 
 
 @pytest.fixture
 def gdf():
-    '''
-    Er zijn 3 thresholds; 10, 100 en 1000 m². (Zie MIN_AREA (TODO: zie config))
+    """
+    Er zijn 3 thresholds; 10, 100 en 1000 m². (Zie min_area (TODO: zie config))
 
     5 vlakken
-    
+
     1. 100% H7220, 10 m²
     2. 100% H1234, 80 m²
     4. 50% H1234, 50% H4321, 100 m²
     5. 100% H9110, 1000 m²
-    '''
+    """
+    # Deze heb ik hier hard gecode omdat anders de test_env_vars_overwrite_config mijn min_area en min_area_default overschrijft
+    os.environ["VEG2HAB_MINIMUM_OPPERVLAK_EXCEPTIONS"] = json.dumps(
+        {
+            "H6110": 10,
+            "H7220": 10,
+            "H2180_A": 1000,
+            "H2180_B": 1000,
+            "H2180_C": 1000,
+            "H9110": 1000,
+            "H9120": 1000,
+            "H9160_A": 1000,
+            "H9160_B": 1000,
+            "H9190": 1000,
+            "H91D0": 1000,
+            "H91E0_A": 1000,
+            "H91E0_B": 1000,
+            "H91E0_C": 1000,
+            "H91F0": 1000,
+        }
+    )
+    os.environ["VEG2HAB_MINIMUM_OPPERVLAK_DEFAULT"] = "100"
+    min_area = CLIInterface.get_instance().get_config().minimum_oppervlak
+    min_area_default = (
+        CLIInterface.get_instance().get_config().minimum_oppervlak_default
+    )
+
     return gpd.GeoDataFrame(
         {
-            "Opp": [MIN_AREA["H6110"], MIN_AREA["H1234"] * 0.8, MIN_AREA["H1234"], MIN_AREA["H2180_A"]],
+            "Opp": [
+                min_area.get("H6110", min_area_default),
+                min_area.get("H1234", min_area_default) * 0.8,
+                min_area.get("H1234", min_area_default),
+                min_area.get("H2180_A", min_area_default),
+            ],
             "VegTypeInfo": [
                 [VegTypeInfo(SBB=[], VvN=[], percentage=100)],
                 [VegTypeInfo(SBB=[], VvN=[], percentage=100)],
-                [VegTypeInfo(SBB=[], VvN=[], percentage=60), VegTypeInfo(SBB=[], VvN=[], percentage=40)],
+                [
+                    VegTypeInfo(SBB=[], VvN=[], percentage=60),
+                    VegTypeInfo(SBB=[], VvN=[], percentage=40),
+                ],
                 [VegTypeInfo(SBB=[], VvN=[], percentage=100)],
             ],
             "HabitatKeuze": [
@@ -64,7 +79,6 @@ def gdf():
                         status=KeuzeStatus.DUIDELIJK,
                         habtype="H7220",
                         kwaliteit=Kwaliteit.GOED,
-                        zelfstandig=True,
                         habitatvoorstellen=[],
                     )
                 ],
@@ -73,7 +87,6 @@ def gdf():
                         status=KeuzeStatus.DUIDELIJK,
                         habtype="H1234",
                         kwaliteit=Kwaliteit.GOED,
-                        zelfstandig=True,
                         habitatvoorstellen=[],
                     )
                 ],
@@ -82,23 +95,20 @@ def gdf():
                         status=KeuzeStatus.DUIDELIJK,
                         habtype="H1234",
                         kwaliteit=Kwaliteit.GOED,
-                        zelfstandig=True,
                         habitatvoorstellen=[],
                     ),
                     HabitatKeuze(
                         status=KeuzeStatus.DUIDELIJK,
                         habtype="H4321",
                         kwaliteit=Kwaliteit.GOED,
-                        zelfstandig=True,
                         habitatvoorstellen=[],
-                    )
+                    ),
                 ],
                 [
                     HabitatKeuze(
                         status=KeuzeStatus.DUIDELIJK,
                         habtype="H9110",
                         kwaliteit=Kwaliteit.GOED,
-                        zelfstandig=True,
                         habitatvoorstellen=[],
                     )
                 ],
@@ -112,14 +122,18 @@ def extract_habtypen(gdf):
 
 
 def test_generic_habtype_minimum_area(gdf):
+    min_area = CLIInterface.get_instance().get_config().minimum_oppervlak
+    min_area_default = (
+        CLIInterface.get_instance().get_config().minimum_oppervlak_default
+    )
     # Te klein
     subset = gdf.iloc[[1]].copy()
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
     assert [["H0000"]] == extract_habtypen(subset)
-    
+
     # Groot genoeg
     subset = gdf.iloc[[1]].copy()
-    subset["area"] = MIN_AREA["H1234"] * 1.1
+    subset["Opp"] = min_area.get("H1234", min_area_default) * 1.1
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
     assert [["H1234"]] == extract_habtypen(subset)
 
@@ -132,12 +146,19 @@ def test_special_cases_minimum_area(gdf):
 
     # Omgewisseld; alleen H7220 is groot genoeg
     subset = gdf.iloc[[0, 3]].copy()
-    subset.HabitatKeuze.at[0], subset.HabitatKeuze.at[3] = subset.HabitatKeuze.at[3], subset.HabitatKeuze.at[0]
+    subset.HabitatKeuze.at[0], subset.HabitatKeuze.at[3] = (
+        subset.HabitatKeuze.at[3],
+        subset.HabitatKeuze.at[0],
+    )
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
     assert [["H0000"], ["H7220"]] == extract_habtypen(subset)
 
 
 def test_complex_minimum_area(gdf):
+    min_area = CLIInterface.get_instance().get_config().minimum_oppervlak
+    min_area_default = (
+        CLIInterface.get_instance().get_config().minimum_oppervlak_default
+    )
     # Precies groot genoeg voor 1 habtype, maar door complex te klein
     subset = gdf.iloc[[2]].copy()
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
@@ -145,18 +166,19 @@ def test_complex_minimum_area(gdf):
 
     # Dubbel zo groot vlak, dus 60% is nu groot genoeg
     subset = gdf.iloc[[2]].copy()
-    subset["area"] = MIN_AREA["H1234"] * 2
+    subset["Opp"] = min_area.get("H1234", min_area_default) * 2
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
     assert [["H1234", "H0000"]] == extract_habtypen(subset)
 
     # Drie keer zo groot vlak, dus 60% en 40% zijn nu groot genoeg
     subset = gdf.iloc[[2]].copy()
-    subset["area"] = MIN_AREA["H1234"] * 3
+    subset["Opp"] = min_area.get("H1234", min_area_default) * 3
     subset["HabitatKeuze"] = apply_minimum_oppervlak(subset)
     assert [["H1234", "H4321"]] == extract_habtypen(subset)
 
 
 def test_all_minimum_area(gdf):
     gdf["HabitatKeuze"] = apply_minimum_oppervlak(gdf)
-    assert [["H7220"], ["H0000"], ["H0000", "H0000"], ["H9110"]] == extract_habtypen(gdf)
-
+    assert [["H7220"], ["H0000"], ["H0000", "H0000"], ["H9110"]] == extract_habtypen(
+        gdf
+    )
